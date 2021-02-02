4ac38fafaac4df5d819845811a911132579399c9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-02 13:35:16 +0100

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-// lesson 135/136
+import Sidebar from './Sidebar'
+// lesson 135/136/137
 function App() {
   return (
     <>
       <Navbar />
+      <Sidebar />
     </>
   )
 }

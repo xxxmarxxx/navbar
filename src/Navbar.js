67f0bf824d0eb0510643ff9479c5f5fe1ed378b6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-02 13:35:55 +0100

    change links.map

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,25 +15,40 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">concact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className="social-icons">
-          <li><a href="https://www.twitter.com"><FaTwitter/></a></li>
-          <li><a href="https://www.twitter.com"><FaTwitter/></a></li>
-          <li><a href="https://www.twitter.com"><FaTwitter/></a></li>
-          
+          {social.map((socialIcon)=>{
+            const {id,url,icon} = socialIcon
+            return(
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+          {/* <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li> */}
         </ul>
       </div>
     </nav>

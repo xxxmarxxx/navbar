2643db65a45196d5956758e9b94a1048f2f3b284
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-02 12:46:45 +0100

    lesson 136 add navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
+// lesson 135/136
 function App() {
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar />
     </>
   )
 }
